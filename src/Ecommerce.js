f4701e8a135414ea7a2fc8bd22a12d6cacdd5e1b
shivangikhoria01 <shivangikhoria01@gmail.com>
2023-10-30 09:21:38 +0530

    changes

---
+++ src/Ecommerce.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import Table from 'react-bootstrap/Table';
+function Ecommerce()
+{
+    const [data,setapidata]=useState([])
+    fetch("https://fakestoreapi.com/products").then((result)=>{
+        //console/log(result)
+        result.json().then((data1)=>{
+            console.log(data1)
+            setapidata(data1)
+        })
+    })
+    return(
+<div>
+<Table striped bordered hover variant="dark">
+<tbody>
+    <tr>
+        <td>ID</td>
+        <td>Title</td>
+        <td>Price</td>
+        <td>Category</td>
+        <td>Image</td>
+    </tr>
+    {
+        data.map((item)=>
+        <tr>
+            <td>{item.id}</td>
+             <td>{item.title}</td>
+             <td>{item.price*80} RS </td>
+             <td>{item.description}</td>
+             <td>{item.category}</td>
+             <td><img src={item.image} width="100px" height="100px"></img></td>
+        </tr>
+        )
+    }   
+</tbody> 
+        </Table>      
+</div>
+)
+    }
+    export default Ecommerce
+
