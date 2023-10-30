8f5976a76540742266c9a17c179061ba32768a39
shivangikhoria01 <shivangikhoria01@gmail.com>
2023-10-30 08:36:48 +0530

    chnages

--- src/ApiData.js
+++ src/ApiData.js
@@ -1,14 +1,37 @@
+import { useState } from "react"
+import Table from 'react-bootstrap/Table';
 function ApiData()
 {
+    const [data,setapidata]=useState([])
     fetch("https://jsonplaceholder.typicode.com/posts").then((result)=>{
         //console/log(result)
-        result.json().then((data)=>{
-            console.log(data)
+        result.json().then((data1)=>{
+            console.log(data1)
+            setapidata(data1)
         })
     })
 return(
     <div>
-        <h1>Api Data</h1>
+        <Table striped bordered hover variant="dark">
+<tbody>
+    <tr>
+        <td>User ID</td>
+        <td> ID</td>
+        <td>Title</td>
+        <td>Body</td>
+    </tr>
+    {
+        data.map((item)=>
+        <tr>
+            <td>{item.userid}</td>
+             <td>{item.id}</td>
+             <td>{item.title}</td>
+             <td>{item.body}</td>
+        </tr>
+        )
+    }   
+</tbody>
+        </Table>      
     </div>
 )
 }
